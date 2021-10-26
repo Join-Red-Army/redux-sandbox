ab42792b0a2523a6ce846bad39602b6c03de596d
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2021-10-26 21:00:42 +0300

    payload - передача дополнительных параметров в функцию reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
+    case 'RND':
+      return state + action.payload;
+      
     case 'INC':
       return state + 1;
 
@@ -15,18 +18,29 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+// увеличить
 document
   .getElementById('inc')
   .addEventListener('click', () => {
     store.dispatch({type: 'INC'});
   });
 
+// уменьшить
 document
   .getElementById('dec')
   .addEventListener('click', () => {
     store.dispatch({type: 'DEC'});
   });
 
+// случайное число
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({type: 'RND', payload});
+  });
+
+// обновить значение
 const update = () => {
   document
     .getElementById('counter')
