f4d51852b3982ae5f5cb00b51670914df386b5a8
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2021-10-28 04:03:05 +0300

    Использование библиотеки reaсt-redux

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import App from './components/app';
 import reducer from './reducer';
-import * as actions from './actions';
-import Counter from './counter';
 
 // создание store
 const store = createStore(reducer);
-// для удобства, dispatch можно деструктуризировать
-const { dispatch } = store;
 
-// bindActionCreators
-const { inc, dec, rnd } = 
-  bindActionCreators(actions, dispatch);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />, 
+  </Provider>,
+  document.getElementById('root')
+);
 
 
-// обновить значение
-const update = () => {
-  ReactDom.render(
-    <Counter
-      counter={store.getState()}
-      inc={inc}
-      dec={dec}
-      rnd={() => {
-        const value = Math.floor(Math.random()*10);
-        rnd(value);  
-      }} />, 
-    document.getElementById('root')
-  );
-};
-
-// первый запуск - вручную
-update();
-// повторные обновления делает store
-store.subscribe(update);
+// Counter
+//       counter={store.getState()}
+//       inc={inc}
+//       dec={dec}
+//       rnd={() => {
+//         const value = Math.floor(Math.random()*10);
+//         rnd(value);  
+//       }}
